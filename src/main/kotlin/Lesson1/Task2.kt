e7f7be08c217cc22e96e5369a1c575e5726363f2
JayGC7 <artemka94-20@mail.ru>
2024-06-16 16:36:49 +0300

    L1_T2 Lesson1 Task2 (init and print values)

---
+++ src/main/kotlin/Lesson1/Task2.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson1
+
+fun main(){
+    val order_num:Int = 75
+    val gratitude_phrase:String = "Thank you!"
+    var workers_num:Int = 2000
+
+    println(order_num)
+    println(gratitude_phrase)
+    //println(workers_num)
+    workers_num -= 1
+    println(workers_num)
+}
