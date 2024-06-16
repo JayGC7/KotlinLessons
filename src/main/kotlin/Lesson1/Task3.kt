83384b3029514744bc6b181999b4223ba9e6231b
JayGC7 <artemka94-20@mail.ru>
2024-06-16 17:09:10 +0300

    L1_T3 Lesson1 Task3 (init, change, print values)

---
+++ src/main/kotlin/Lesson1/Task3.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson1
+
+fun main(){
+    val year: Int = 1963
+    var hour:Int = 9
+    var minute: Int = 7
+
+    println( "Year of Gagarin's flight: " + year )
+    println( "Hour when Gagarin flew: " + hour )
+    println( "Minute of hour when Gagarin flew: " + minute )
+
+    hour = 10
+    minute = 55
+    print( "Gagarin landed at " + hour + ":" + minute )
+}
