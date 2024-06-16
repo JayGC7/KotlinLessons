6f2cad4f52d9ee7d431dc3df89470a21b6073a0c
JayGC7 <artemka94-20@mail.ru>
2024-06-16 17:46:44 +0300

    L1_T4 Lesson1 Task4 (init values with minsize type)

---
+++ src/main/kotlin/Lesson1/Task4.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson1
+
+fun main(){
+    val length:Long = 40868600000
+    val age:Byte = 27
+    val partOfDay:Float = 0.075F
+    val seconds:Short = 6480
+    val partOfYear:Double = 2.0547945205479453e-4
+    val apogee:Int = 327000
+
+    println("length: " + length)
+    println("age: " + age)
+    println("partOfDay: " + partOfDay)
+    println("seconds: " + seconds)
+    println("partOfYear: " + partOfYear)
+    println("apogee: " + apogee)
+
+}
