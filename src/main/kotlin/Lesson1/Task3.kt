63d1c4521dc28777971978c5a09dfd6692746336
JayGC7 <artemka94-20@mail.ru>
2024-06-17 20:20:39 +0300

    L1_T3 Lesson1 Task3 (add leading zero)

--- src/main/kotlin/Lesson1/Task3.kt
+++ src/main/kotlin/Lesson1/Task3.kt
@@ -1,15 +1,18 @@
 package org.example.Lesson1
 
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 fun main(){
     val year: Int = 1963
     var hour:Int = 9
     var minute: Int = 7
 
     println( "Year of Gagarin's flight: " + year )
-    println( "Hour when Gagarin flew: " + hour )
-    println( "Minute of hour when Gagarin flew: " + minute )
+    println( "Hour when Gagarin flew: " + hour.toString().padStart(2, '0') )
+    println( "Minute of hour when Gagarin flew: " + minute.toString().padStart(2, '0') )
 
     hour = 10
     minute = 55
-    print( "Gagarin landed at " + hour + ":" + minute )
+    println("Gagarin landed at " + LocalTime.of(hour,minute).format(DateTimeFormatter.ofPattern("HH:mm")))
 }
