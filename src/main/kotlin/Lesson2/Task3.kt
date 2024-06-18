553df56467e426b0448d484aee3252d45beb05be
JayGC7 <artemka94-20@mail.ru>
2024-06-18 21:21:07 +0300

    L2_T3 Lesson2 Task3 (refactor global constant)

--- src/main/kotlin/Lesson2/Task3.kt
+++ src/main/kotlin/Lesson2/Task3.kt
@@ -3,18 +3,17 @@ package org.example.Lesson2
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
-fun main(){
-    val start_hour:Int = 9
-    val start_min:Int = 39
-    val road_time:Int = 457
-    val sixty = 60
+const val SEC_IN_MINUTE = 60
+fun main() {
+    val start_hour: Int = 9
+    val start_min: Int = 39
+    val road_time: Int = 457
 
-    val start_time = start_hour*sixty + start_min
+    val start_time = start_hour * SEC_IN_MINUTE + start_min
     val arrive_time = start_time + road_time
 
-    val arrive_hour = arrive_time/sixty
-    val arrive_min = arrive_time%sixty
+    val arrive_hour = arrive_time / SEC_IN_MINUTE
+    val arrive_min = arrive_time % SEC_IN_MINUTE
 
-    println("Train arrives at: " + LocalTime.of(arrive_hour,arrive_min).format(DateTimeFormatter.ofPattern("HH:mm")))
-
+    println("Train arrives at: " + LocalTime.of(arrive_hour, arrive_min).format(DateTimeFormatter.ofPattern("HH:mm")))
 }
