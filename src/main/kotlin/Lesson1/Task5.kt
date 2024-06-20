b6c32f5eb97a8005777fe6cbf221b7c89c432e50
JayGC7 <artemka94-20@mail.ru>
2024-06-20 14:36:49 +0300

    L1_T5 Lesson1 Task5 (отступы)

--- src/main/kotlin/Lesson1/Task5.kt
+++ src/main/kotlin/Lesson1/Task5.kt
@@ -1,7 +1,10 @@
 package org.example.Lesson1
+
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+
 const val SEC_IN_MINUTE =60
+
 fun main(){
     var sec = 6480
     var min = sec / SEC_IN_MINUTE
@@ -10,7 +13,4 @@ fun main(){
     val min_in_space = min % SEC_IN_MINUTE
 
     println(LocalTime.of(hour,min_in_space,sec_in_space).format(DateTimeFormatter.ofPattern("HH:mm:ss")))
-
-
-
 }
