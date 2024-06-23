cd34b5078a5986f98368b95996c6801c1f20bbbf
JayGC7 <artemka94-20@mail.ru>
2024-06-23 20:28:01 +0300

    L4_T2 Lesson4 Task2 (comparing with average)

---
+++ src/main/kotlin/Lesson4/Task2.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson4
+
+fun main(){
+    val average_weight_min = 35
+    val average_weight_max = 100
+    val average_volume = 100
+
+    var package_weight = 20
+    var package_volume = 80
+
+    println("Package with $package_weight weight and $package_volume volume corresponds to average: " +
+            (average_weight_min < package_weight && average_weight_max >= package_weight && average_volume > package_volume) )
+
+    package_weight = 50
+    package_volume = 100
+    println("Package with $package_weight weight and $package_volume volume corresponds to average: " +
+            (average_weight_min < package_weight && average_weight_max >= package_weight && average_volume > package_volume) )
+}
