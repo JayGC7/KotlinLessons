84b9da8efa4dbe4b8600e8c35bc252252ccc184a
JayGC7 <artemka94-20@mail.ru>
2024-06-23 20:15:48 +0300

    L4_T5 Lesson4 Task1 (comparing)

---
+++ src/main/kotlin/Lesson4/Task1.kt
@@ -0,0 +1,23 @@
+package org.example.Lesson4
+
+fun main(){
+    val tables = 13
+    val today_reserved = 13
+    val tomorrow_reserved = 9
+    var free_to_reserved:Boolean
+
+    if (tables > today_reserved) {
+        free_to_reserved = true
+    }else{
+        free_to_reserved = false
+    }
+    println("Free to reserved today: $free_to_reserved")
+
+    if (tables > tomorrow_reserved) {
+        free_to_reserved = true
+    }else{
+        free_to_reserved = false
+    }
+    println("Free to reserved tomorrow: $free_to_reserved")
+
+}
