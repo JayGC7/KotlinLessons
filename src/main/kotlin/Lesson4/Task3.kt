358c54a240b1ab1701b96572cdf397d8536368f1
JayGC7 <artemka94-20@mail.ru>
2024-06-23 20:36:06 +0300

    L4_T3 Lesson4 Task3 (comparing condition)

---
+++ src/main/kotlin/Lesson4/Task3.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson4
+
+fun main(){
+    val sun_condition = true
+    val open_tent_condition = true
+    val humidity_condition = 20
+    val bad_season = "winter"
+
+    var sun_today = true
+    var open_tent_today = true
+    var humidity_today = 20
+    var season_today = "winter"
+
+    val analysis = (sun_today == sun_condition) && (open_tent_today == open_tent_condition) && (humidity_today == humidity_condition) && (season_today != bad_season) && (open_tent_today == open_tent_condition)
+    println("Are the conditions favorable for the growth of legumes now? $analysis")
+}
