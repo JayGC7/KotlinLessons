9d6a02157a77f15c27508fbb528896838bee5cb1
JayGC7 <artemka94-20@mail.ru>
2024-06-24 11:18:26 +0300

    L3_T3 Lesson3 Task3 ()

--- src/main/kotlin/Lesson3/Task3.kt
+++ src/main/kotlin/Lesson3/Task3.kt
@@ -1,10 +1,10 @@
 package org.example.Lesson3
 
-fun main(){
-    val number: Int = readLine()!!.toInt()
+fun main() {
+    val number = 4
     var out_string: String = ""
     var result = 0
-    for (i in 1..10){
+    for (i in 1..10) {
         result = number * i
         out_string += "$number x $i = $result\n"
     }
