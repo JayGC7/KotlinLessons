2d1b5a486dd7085a241f43779e84c314b5cc8405
JayGC7 <artemka94-20@mail.ru>
2024-06-24 11:09:51 +0300

    L3_T2 Lesson3 Task3 (print personal data)

--- src/main/kotlin/Lesson3/Task2.kt
+++ src/main/kotlin/Lesson3/Task2.kt
@@ -5,23 +5,13 @@ fun main(){
     var family_name = "Andreeva"
     var personal_name = "Tatiana"
     var father_name = "Sergeevna"
-    var age = 0
-    var changes = mutableMapOf(age to "$family_name $personal_name $father_name")
+    var age = 20
+    var changes = mutableListOf("$family_name $personal_name $father_name, $age")
 
     age = 22
     family_name = "Sidorova"
-    changes.put(age, "$family_name $personal_name $father_name")
+    changes.addLast("$family_name $personal_name $father_name, $age")
 
-    val iterator = changes.keys.toList()
-    for (i in listOf(20,22)){
-        for (j in 0..(iterator.size-1)){
-            if (iterator[j]==iterator.last()){
-                println(changes[iterator.last()] + ", " + i)
-                break
-            }else if (iterator[j]<=i && iterator[j+1]> i ){
-                println(changes[j]+ ", " + i)
-                break
-            }
-        }
-    }
+    println(changes[0])
+    println(changes[1])
 }
