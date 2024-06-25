328ee317676939de37cace62cc781638b1f39806
JayGC7 <artemka94-20@mail.ru>
2024-06-25 10:00:20 +0300

    L3_T5 Lesson3 Task5 (use split to parse string)

--- src/main/kotlin/Lesson3/Task5.kt
+++ src/main/kotlin/Lesson3/Task5.kt
@@ -2,9 +2,11 @@ package org.example.Lesson3
 
 fun main(){
     val in_data = "D2:D4;0"
-    var move_from = in_data.substring(0, in_data.indexOf(":"))
-    var move_to = in_data.substring(in_data.indexOf(":")+1, in_data.indexOf(";"))
-    var move_num = in_data.substring(in_data.indexOf(";")+1, in_data.length)
+    val split_in_data = in_data.split(':',';')
+
+    var move_from = split_in_data[0]
+    var move_to = split_in_data[1]
+    var move_num = split_in_data[2]
 
     println("Move from: $move_from")
     println("Move to: $move_to")
