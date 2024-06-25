936e8bb069fae06772eeba3176a70de386099727
JayGC7 <artemka94-20@mail.ru>
2024-06-25 09:26:04 +0300

    L4_T1 Lesson4 Task1 (refactor code design)

--- src/main/kotlin/Lesson4/Task1.kt
+++ src/main/kotlin/Lesson4/Task1.kt
@@ -1,21 +1,21 @@
 package org.example.Lesson4
 
-fun main(){
+fun main() {
     val tables = 13
     val today_reserved = 13
     val tomorrow_reserved = 9
-    var free_to_reserved:Boolean
+    var free_to_reserved: Boolean
 
     if (tables > today_reserved) {
         free_to_reserved = true
-    }else{
+    } else {
         free_to_reserved = false
     }
     println("Free to reserved today: $free_to_reserved")
 
     if (tables > tomorrow_reserved) {
         free_to_reserved = true
-    }else{
+    } else {
         free_to_reserved = false
     }
     println("Free to reserved tomorrow: $free_to_reserved")
